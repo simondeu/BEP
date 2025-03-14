965f598bf4002c26ca2ecb4483607cbf3eb8a698
simondeu <104424674+simondeu@users.noreply.github.com>
2025-03-14 15:52:34 +0100

    nieuwe code

---
+++ BlobTreeConstructor.py
@@ -0,0 +1,102 @@
+class Node():
+    
+    def __init__(self, name, neighbours):
+        self.name = name
+        self.neighbours = neighbours
+        
+    def __str__(self):
+        return self.name
+    
+    def __name__(self):
+        return self.name
+    
+class Edge():
+    
+    def __init__(self, u, v):
+        self.u = u
+        self.v = v
+        self.name = u.name + v.name
+
+def GetAB(u,v,A=[]):
+    A.append(u)
+    for node in u.neighbours:
+        if node not in A and node != v:
+            A = GetAB(node,u,A)
+    return A
+
+def CheckABx(A,B,Leaf):
+    for i in A:
+        for x1 in B:
+            for x2 in B:
+                if x1 != x2:
+                    if ((i,Leaf),(x1,x2)) not in Splits:
+                        return False
+    return True
+
+def FindStem(Leaf):
+    for Edge in NetworkEdges:
+        A = GetAB(Edge.u,Edge.v)
+        B = GetAB(Edge.v,Edge.u)
+        Ac = CheckABx(A,B,Leaf)
+        Bc = CheckABx(B,A,Leaf)
+        
+        
+def ConstructNetwork():
+    pass
+        
+
+
+#Reading the file and constructing a set of leaves and a set of the splits of the network
+
+with open( "Test.txt") as f:
+    lines = f.readlines()
+
+Leaves = []
+TempLeaves = lines[0].split("(")[1].split(")")[0].split(",")
+for i in TempLeaves:
+    Leaves.append(Node(i,[]))
+Splits = []
+for i in range(1,len(lines)):
+    line = lines[i]
+    Splits.append(((line[1],line[3]),(line[5],line[7])))
+    
+    
+
+NetworkLeaves = []
+NetworkEdges = []
+InternalNodes = 0
+
+#Step 1: Creating the first edge of the network
+Leaves[0].neighbours.append(Leaves[1]), NetworkLeaves.append(Leaves[0])
+Leaves[1].neighbours.append(Leaves[0]), NetworkLeaves.append(Leaves[1])
+NetworkEdges.append(Edge(Leaves[0],Leaves[1]))
+
+Leaves[2].neighbours.append(Leaves[0]), Leaves[0].neighbours.append(Leaves[2])
+
+#Step 2: Find stem for next leaf
+
+for i in range(1,len(Leaves)-1):
+    FindStem(Leaves[i+1])
+    ConstructNetwork()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
